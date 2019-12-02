0c6bbb8fc7620354d093bec0eb04119bd4d2339d
Pascal <pascal@luther.ch>
2019-12-02 10:04:30 +0800

    updated dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,11 +36,7 @@ export default {
     [
       'nuxt-fire',
       {
-        useOnly: ['firestore'],
-        customEnv: false,
-        functionsLocation: 'us-central1',
         config: {
-          development: {
             apiKey: 'AIzaSyDG_gasyorLNjDv6eBmeSnV321WHkCODlU',
             authDomain: 'nuxt-fire-vuexfire.firebaseapp.com',
             databaseURL: 'https://nuxt-fire-vuexfire.firebaseio.com',
@@ -48,18 +44,11 @@ export default {
             storageBucket: '',
             messagingSenderId: '1048167249658',
             appId: '1:1048167249658:web:d45b765280a64ed4'
-          },
-          production: {
-            apiKey: 'AIzaSyDG_gasyorLNjDv6eBmeSnV321WHkCODlU',
-            authDomain: 'nuxt-fire-vuexfire.firebaseapp.com',
-            databaseURL: 'https://nuxt-fire-vuexfire.firebaseio.com',
-            projectId: 'nuxt-fire-vuexfire',
-            storageBucket: '',
-            messagingSenderId: '1048167249658',
-            appId: '1:1048167249658:web:d45b765280a64ed4'
-          }
-        }
+        },
+        services: {
+          firestore: true
       }
+    } 
     ]
   ]
 }
