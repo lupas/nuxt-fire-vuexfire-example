769b659cd18d963543705a156a5c033c14940c01
Pascal Luther <pascal@luther.ch>
2020-04-17 23:11:25 +0200

    add unbindCountDocument

--- store/index.js
+++ store/index.js
@@ -2,22 +2,25 @@ import { vuexfireMutations, firestoreAction } from 'vuexfire'
 
 export const state = () => ({
   countDocument: {
-    count: null
-  }
+    count: null,
+  },
 })
 export const mutations = {
-  ...vuexfireMutations
+  ...vuexfireMutations,
 }
 export const actions = {
-  bindCountDocument: firestoreAction(async function({ bindFirestoreRef }) {
+  bindCountDocument: firestoreAction(async function ({ bindFirestoreRef }) {
     const ref = this.$fireStore
       .collection('countCollection')
       .doc('countDocument')
     await bindFirestoreRef('countDocument', ref, { wait: true })
-  })
+  }),
+  unbindCountDocument: firestoreAction(function ({ unbindFirestoreRef }) {
+    unbindFirestoreRef('countDocument')
+  }),
 }
 export const getters = {
   count(state) {
     return state.countDocument.count
-  }
+  },
 }
