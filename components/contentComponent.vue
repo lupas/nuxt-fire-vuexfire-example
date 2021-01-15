19cfaabaa6c8d449703be3cedd85f79234130cef
Pascal Luther <pascal@luther.ch>
2021-01-15 09:44:04 +0100

    chore(deps): Update dependencies

--- components/contentComponent.vue
+++ components/contentComponent.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">
-        nuxt-fire-vuexfire-example
-      </h1>
+      <h1 class="title">nuxt-fire-vuexfire-example</h1>
       <h2 class="subtitle">
         Example project showcasing the use of @nuxtjs/firebase together with
         vuexfire.
       </h2>
-      <h3>
-        Count
-      </h3>
+      <h3>Count</h3>
       <p class="count">{{ count }}</p>
       <div class="links">
         <div class="button--green" @click="changeCount(-1)">-1</div>
